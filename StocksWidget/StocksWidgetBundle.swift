826af12b9eff5c0661c0fad46c6f657ebcde8a9c
Takasur Azeem <takasurazeem@gmail.com>
2025-03-15 23:18:52 +0500

    Configures the intent.

---
+++ StocksWidget/StocksWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StocksWidgetBundle.swift
+//  StocksWidget
+//
+//  Created by Takasur Azeem on 3/15/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StocksWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StocksWidget()
+    }
+}
